e6f09e41b1657d58a01cb9ba967f96142c346f98
Vladimir <v.tyrin@icloud.com>
2021-05-04 15:16:15 +0300

    server-side changes

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,29 @@
-var express = require('express'),
-    multer = require('multer'),
-    fs = require('fs'),
-    router = express.Router();
-router.get('/', function (req, res, next) {
-    res.render('/public/index.html', {title: 'files'});
+var express = require("express"),
+  multer = require("multer"),
+  fs = require("fs"),
+  router = express.Router();
+router.get("/", function (req, res, next) {
+  res.render("/public/index.html", { title: "files" });
 });
 var upload = multer({
-    dest: '../../html/uploads/',
-    limits: {
-        files: 250,
-        fileSize: 1 * 1024 * 1024 * 1024
-    }
+  dest: "../html/uploads/",
+  limits: {
+    files: 250,
+    fileSize: 1 * 1024 * 1024 * 1024,
+  },
 });
-router.post('/upload/', upload.any(), function (req, res) {
-    var path = req.files[0].path;
-    var pathNew = path + '.' + req
-        .files[0]
-        .originalname
-        .split('.')
-        .pop();
-    console.log(pathNew);
-    switch (req.files[0].mimetype) {
-        default:
-            fs.rename(path, pathNew, function (err) {
-                if (err) 
-                    throw err;
-                }
-            );
-            break;
-    }
+router.post("/upload/", upload.any(), function (req, res) {
+  var path = req.files[0].path;
+  var pathNew = path + "_" + req.files[0].originalname;
+  console.debug(req.files[0]);
 
-    res.type('json');
-    const link = pathNew.replace('../../html/', '')
-    res.json({link, type: req.files[0].mimetype});
-    res.end();
+  fs.rename(path, pathNew, function (err) {
+    if (err) throw err;
+  });
+
+  res.type("json");
+  const link = pathNew.replace("../html/", "");
+  res.json({ link, type: req.files[0].mimetype });
+  res.end();
 });
 module.exports = router;
