94d5b64a3c0935e034ac8f7ee13c77096ce27b86
Vladimir Tyrin <vtyrin@in-technology.de>
2021-05-05 09:11:31 +0700

    debug

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var favicon = require("serve-favicon");
 var routes = require("./routes/index");
 var app = express();
 
-console.info("files app working dir", __dirname);
 app.use(favicon(__dirname + "/public/favicon.ico"));
 app.set("view engine", "html");
+app.set("views", __dirname);
 app.engine("html", require("ejs").renderFile);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routes);
 
 app.use(function (req, res, next) {
+  console.log(req);
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
@@ -24,6 +25,7 @@ app.use(function (err, req, res, next) {
 
 var server = app.listen(4000, function () {
   var port = server.address().port;
+  console.info("files app working dir", __dirname);
   console.info("files app listening at http://localhost:%s", port);
 });
 
